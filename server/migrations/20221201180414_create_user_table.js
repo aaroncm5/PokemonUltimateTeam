f527d6cb728718a5800ed4a9910f69e8e5025c36
Aaron Mahabir <aaroncurtis32@gmail.com>
2022-12-01 13:42:58 -0500

    create table migration file for user

--- server/migrations/20221201180414_create_user_table.js
+++ server/migrations/20221201180414_create_user_table.js
@@ -1,15 +1,13 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary();
+    table.string('user_name').notNullable();
+    table.string('user_username').notNullable();
+    table.string('user_email').notNullable();
+    table.string('user_password').notNullable();
+  })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('users');
 };
